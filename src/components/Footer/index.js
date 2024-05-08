53e5841024b34e1379eb8e5a3f6dd792d91bd12d
diegosantos-engtads <diegosantos.engtads@gmail.com>
2024-05-08 18:23:24 -0300

    test: movie project started

---
+++ src/components/Footer/index.js
@@ -0,0 +1,10 @@
+import styles from "./Footer.module.css"
+
+function Footer() {
+    return (
+        <footer className={styles.header}>
+            <h2>LelaFlix &copy;Desenvolvido por Diego Santos 2024</h2>
+        </footer>
+    );
+}
+export default Footer;
